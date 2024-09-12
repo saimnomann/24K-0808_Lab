93bca9cc035e991efadd8e80c0be638f05630ac0
saimnomann <141673697+saimnomann@users.noreply.github.com>
2024-09-12 15:23:23 +0500

    Add files via upload

---
+++ q1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int num;
+printf("Please enter a number to check multiple of 3");
+scanf("%d",&num);
+if(num%3==0){
+printf("It is a multiple of 3");
+}
+else{
+printf("It is not a multiple of 3");}
+}
---
+++ q2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+int num;
+printf("Please enter a number greater than 0 which is a multiple of 2 and divisble by 10");
+scanf("%d",&num);
+if(num>=0 && num%2==0 && num%10 ==0){
+printf("It is positive, multiple of 2 and divisible by 10");
+}
+else{
+printf("It is positive, multiple of 2 and divisible by 10");
+}
+}
